a6d8c6f7d0e638bc9f880435040d1f50f02720b9
jihadabuzuhri <jihadabuzuhri@gmail.com>
2022-12-03 17:34:53 +0200

    Tax_Requirement

---
+++ PriceCalculatorKata/Calculation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PriceCalculatorKata
+{
+    class Calculation
+    {
+        public static Product AddTax(Product product , double taxRate)
+        {
+            return new Product
+            (
+                product.Name,
+                product.UPC,
+                product.Price + Math.Round(product.Price * (taxRate/100) , 2)
+            );
+
+        }
+    }
+}
--- PriceCalculatorKata/Program.cs
+++ PriceCalculatorKata/Program.cs
@@ -6,7 +6,15 @@ namespace PriceCalculatorKata
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Product Book = new Product("The Little Prince", 12345, 20.25);
+
+            Product BookWithTax = Calculation.AddTax(Book,20);
+            Console.WriteLine($"Product price reported as {Book.Price} before tax and {BookWithTax.Price}  after 20 % tax");
+
+            BookWithTax = Calculation.AddTax(Book, 21);
+            Console.WriteLine($"Product price reported as {Book.Price} before tax and {BookWithTax.Price}  after 21 % tax");
+
         }
     }
 }
