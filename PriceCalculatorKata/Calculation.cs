69d41c3282342bbc301375237a4261784abd58e4
jihadabuzuhri <jihadabuzuhri@gmail.com>
2022-12-04 09:56:07 +0200

    SELECTIVE_Requirement

--- PriceCalculatorKata/Calculation.cs
+++ PriceCalculatorKata/Calculation.cs
@@ -7,18 +7,20 @@ namespace PriceCalculatorKata
     class Calculation
     {
         public static double TaxAmount  { get; set; }
-        public static double DiscountAmount { get; set; }
+        public static double TotalDiscountAmount { get; set; }
         public static double FinalPrice { get; set; }
 
 
-        public static Product AddTaxWithDiscount(Product product , double TaxRate, double DiscountRate)
+        public static Product CalculateFinalPrice(Product product , double TaxRate, double UniversalDiscountRate, double UPC_DiscountRate, int UPC_ForDiscount )
         {
+            double UniversalDiscountAmount = Math.Round(product.Price * (UniversalDiscountRate / 100), 2);
+            double UPC_DiscountAmount= (UPC_ForDiscount==product.UPC)? Math.Round(product.Price * (UPC_DiscountRate / 100), 2) : 0 ;
 
             TaxAmount = Math.Round(product.Price * (TaxRate / 100), 2);
-            DiscountAmount = Math.Round(product.Price * (DiscountRate / 100), 2);
-            FinalPrice = Math.Round(product.Price + TaxAmount - DiscountAmount, 2);
+            TotalDiscountAmount = UniversalDiscountAmount + UPC_DiscountAmount;
+            FinalPrice = Math.Round(product.Price + TaxAmount - TotalDiscountAmount, 2);
             
-            Report.DiscountedReport(FinalPrice, DiscountAmount);
+            Report.DiscountedReport(FinalPrice, TotalDiscountAmount);
 
             return new Product
             (
--- PriceCalculatorKata/Program.cs
+++ PriceCalculatorKata/Program.cs
@@ -8,14 +8,13 @@ namespace PriceCalculatorKata
         {
 
             Product Book = new Product("The Little Prince", 12345, 20.25);
-            Console.WriteLine("Sample product: Book with name = “The Little Prince”, UPC=12345, price=$20.25.");
+            Console.WriteLine("Sample product: Book with name = “The Little Prince”, UPC=12345, price=$20.25.\n");
 
-            Console.WriteLine("Tax = 20%, discount = 15%");
-            Product Book1WithTaxAndDiscount = Calculation.AddTaxWithDiscount(Book,20,15);
+            Console.WriteLine("\nTax = 20%, universal discount = 15%, UPC-discount = 7% for UPC=12345");
+            Product Book1WithTaxAndDiscount = Calculation.CalculateFinalPrice(Book,20,15,7,12345);
 
-            Console.WriteLine("Tax = 20%, no discount");
-            Product Book2WithTaxAndDiscount = Calculation.AddTaxWithDiscount(Book, 20, 0);
-
+            Console.WriteLine("\nTax = 21%, universal discount = 15%, UPC-discount = 7 for UPC = 789");
+            Product Book2WithTaxAndDiscount = Calculation.CalculateFinalPrice(Book, 21, 15,7,789);
 
 
         }
