e35945a1e0163d26e7b811bb8ac50bfb2e77516d
jihadabuzuhri <jihadabuzuhri@gmail.com>
2022-12-10 02:08:54 +0200

    PRECEDENCE_Requirement

--- PriceCalculatorKata/Calculation.cs
+++ PriceCalculatorKata/Calculation.cs
@@ -10,16 +10,95 @@ namespace PriceCalculatorKata
         public static double TotalDiscountAmount { get; set; }
         public static double FinalPrice { get; set; }
 
+        public static double UniversalDiscountAmount { get; set; }
+        public static double UPC_DiscountAmount { get; set; }
 
-        public static Product CalculateFinalPrice(Product product , double TaxRate, double UniversalDiscountRate, double UPC_DiscountRate, int UPC_ForDiscount )
+
+        public static Product CalculateFinalPrice(Product product , double TaxRate, 
+            double UniversalDiscountRate,bool IsUniversalDiscountBeforeTax,
+            double UPC_DiscountRate, bool IsUPC_DiscountBeforeTax, int UPC_ForDiscount)
         {
-            double UniversalDiscountAmount = Math.Round(product.Price * (UniversalDiscountRate / 100), 2);
-            double UPC_DiscountAmount= (UPC_ForDiscount==product.UPC)? Math.Round(product.Price * (UPC_DiscountRate / 100), 2) : 0 ;
 
-            TaxAmount = Math.Round(product.Price * (TaxRate / 100), 2);
-            TotalDiscountAmount = UniversalDiscountAmount + UPC_DiscountAmount;
-            FinalPrice = Math.Round(product.Price + TaxAmount - TotalDiscountAmount, 2);
-            
+
+            if (IsUniversalDiscountBeforeTax&& IsUPC_DiscountBeforeTax)
+            {
+
+                FinalPrice = Math.Round(product.Price, 2);
+
+                UPC_DiscountAmount = (UPC_ForDiscount == product.UPC) ? Math.Round(FinalPrice * (UPC_DiscountRate / 100), 2) : 0;
+                FinalPrice = Math.Round(FinalPrice - UPC_DiscountAmount, 2);
+
+                UniversalDiscountAmount = Math.Round(FinalPrice * (UniversalDiscountRate / 100), 2);
+                FinalPrice = Math.Round(FinalPrice - UniversalDiscountAmount, 2);
+
+
+                TaxAmount = Math.Round(FinalPrice * (TaxRate / 100), 2);
+
+                FinalPrice = Math.Round(FinalPrice + TaxAmount, 2);
+
+
+
+
+
+            }
+            else if (IsUniversalDiscountBeforeTax && IsUPC_DiscountBeforeTax==false)
+            {
+                FinalPrice = Math.Round(product.Price, 2);
+
+                UniversalDiscountAmount = Math.Round(FinalPrice * (UniversalDiscountRate / 100), 2);
+                FinalPrice = Math.Round(FinalPrice - UniversalDiscountAmount, 2);
+
+                TaxAmount = Math.Round(FinalPrice * (TaxRate / 100), 2);
+
+                UPC_DiscountAmount = (UPC_ForDiscount == product.UPC) ? Math.Round(FinalPrice * (UPC_DiscountRate / 100), 2) : 0;
+
+                FinalPrice = Math.Round(FinalPrice - UPC_DiscountAmount, 2);
+
+                FinalPrice = Math.Round(FinalPrice + TaxAmount, 2);
+                TotalDiscountAmount = Math.Round(UniversalDiscountAmount + UPC_DiscountAmount, 2);
+
+            }
+            else if (IsUniversalDiscountBeforeTax ==false && IsUPC_DiscountBeforeTax)
+            {
+                FinalPrice = Math.Round(product.Price, 2);
+
+                UPC_DiscountAmount = (UPC_ForDiscount == product.UPC) ? Math.Round(FinalPrice * (UPC_DiscountRate / 100), 2) : 0;
+                FinalPrice = Math.Round(FinalPrice - UPC_DiscountAmount, 2);
+
+                TaxAmount = Math.Round(FinalPrice * (TaxRate / 100), 2);
+
+                UniversalDiscountAmount = Math.Round(FinalPrice * (UniversalDiscountRate / 100), 2);
+                FinalPrice = Math.Round(FinalPrice - UniversalDiscountAmount, 2);
+
+
+
+                FinalPrice = Math.Round(FinalPrice + TaxAmount, 2);
+                TotalDiscountAmount = Math.Round(UniversalDiscountAmount + UPC_DiscountAmount, 2);
+
+            }
+
+            else if (IsUniversalDiscountBeforeTax ==false && IsUPC_DiscountBeforeTax==false)
+            {
+
+                TaxAmount = Math.Round(FinalPrice * (TaxRate / 100), 2);
+
+                FinalPrice = Math.Round(product.Price, 2);
+
+                UPC_DiscountAmount = (UPC_ForDiscount == product.UPC) ? Math.Round(FinalPrice * (UPC_DiscountRate / 100), 2) : 0;
+                FinalPrice = Math.Round(FinalPrice - UPC_DiscountAmount, 2);
+
+                UniversalDiscountAmount = Math.Round(FinalPrice * (UniversalDiscountRate / 100), 2);
+                FinalPrice = Math.Round(FinalPrice - UniversalDiscountAmount, 2);
+
+                FinalPrice = Math.Round(FinalPrice + TaxAmount, 2);
+                TotalDiscountAmount = Math.Round(UniversalDiscountAmount + UPC_DiscountAmount, 2);
+
+
+            }
+
+
+
+
             Report.DiscountedReport(FinalPrice, TotalDiscountAmount);
 
             return new Product
--- PriceCalculatorKata/Program.cs
+++ PriceCalculatorKata/Program.cs
@@ -8,15 +8,14 @@ namespace PriceCalculatorKata
         {
 
             Product Book = new Product("The Little Prince", 12345, 20.25);
-            Console.WriteLine("Sample product: Book with name = “The Little Prince”, UPC=12345, price=$20.25.\n");
+            Console.WriteLine("Sample product: Book with name = “The Little Prince”," +
+                " UPC=12345, price=$20.25.\n");
 
-            Console.WriteLine("\nTax = 20%, universal discount = 15%, UPC-discount = 7% for UPC=12345");
-            Product Book1WithTaxAndDiscount = Calculation.CalculateFinalPrice(Book,20,15,7,12345);
+            Console.WriteLine("\nTax = 20%, universal discount (after tax) = 15%, UPC-discount (before tax) = 7% for UPC=12345");
+            Product Book1WithTaxAndDiscount = Calculation.CalculateFinalPrice(Book,20,15,false,7,true,12345);
 
-            Console.WriteLine("\nTax = 21%, universal discount = 15%, UPC-discount = 7 for UPC = 789");
-            Product Book2WithTaxAndDiscount = Calculation.CalculateFinalPrice(Book, 21, 15,7,789);
-
-
+            Console.ReadLine();
+            
         }
     }
 }
